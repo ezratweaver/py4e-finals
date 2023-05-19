39b8c388b7d5c447f2cdc6634fc5087de52106dc
ezratweaver <ezratweaver@gmail.com>
2023-05-19 18:04:33 -0500

    Fixed Whitespace

--- polygon_area_calculator/shape_calculator.py
+++ polygon_area_calculator/shape_calculator.py
@@ -34,25 +34,24 @@ class Rectangle():
 
     def get_amount_inside(self, shape):
         return int(self.get_area() / shape.get_area())
-        
-        
+
 
 class Square(Rectangle):
-    
+
     def __init__(self, side):
         self.side = side
         self.set_side(side)
 
     def __str__(self):
         return f"Square(side={self.side})"
-    
+
     def set_side(self, side):
         self.side = side
         self.width = side
         self.height = side
-    
+
     def set_height(self, height):
         self.side = height
-    
+
     def set_width(self, width):
         self.side = width

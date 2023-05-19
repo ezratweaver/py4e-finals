ee1a8f434ee3768f6a352444f878079eae835530
ezratweaver <ezratweaver@gmail.com>
2023-05-19 13:28:03 -0500

    Added Rectangle Methods

--- polygon_area_calculator/shape_calculator.py
+++ polygon_area_calculator/shape_calculator.py
@@ -1,6 +1,28 @@
-class Rectangle:
+class Rectangle():
+    def __init__(self, width, height):
+        self.set_width(width)
+        self.set_height(height)
 
+    def set_width(self, width):
+        self.width = width
 
+    def set_height(self, height):
+        self.height = height
 
+    def get_area(self):
+        return self.width * self.height
 
-class Square:
+    def get_perimeter(self):
+        pass
+
+    def get_diagonal(self):
+        pass
+
+    def get_picture(self):
+        pass
+
+    def get_amount_inside(self):
+        pass
+
+class Square():
+    pass

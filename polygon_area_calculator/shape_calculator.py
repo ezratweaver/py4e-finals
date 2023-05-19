0b16ee11e0e62067d3cc5258527a117318875119
ezratweaver <ezratweaver@gmail.com>
2023-05-19 17:30:41 -0500

    Added Method Functionality

--- polygon_area_calculator/shape_calculator.py
+++ polygon_area_calculator/shape_calculator.py
@@ -13,16 +13,24 @@ class Rectangle():
         return self.width * self.height
 
     def get_perimeter(self):
-        pass
+        return 2 * self.width + 2 * self.height
 
     def get_diagonal(self):
-        pass
+        return (self.width ** 2 + self.height ** 2) ** .5
 
     def get_picture(self):
-        pass
+        if self.height > 50 or self.width > 50:
+            return "Too big for picture."
+        picture = ""
+        for _ in range(self.height):
+            row = "*" * self.width
+            row = row + "\n"
+            picture = picture + row
+        return picture
 
     def get_amount_inside(self):
         pass
 
 class Square():
     pass
+
